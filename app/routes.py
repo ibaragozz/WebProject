e3891f3aa8b42bd14af2938b1821b101e79054fc
ibaragozz <baragozz1@gmail.com>
2024-10-24 19:00:31 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from app import app, db, bcrypt
 from app.models import User
 from app.forms import RegistrationForm, LoginForm
